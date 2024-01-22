841937ea69059439ef08281b18c000b3d8e99b1d
FrancoMuVa <muvafranco@gmail.com>
2024-01-22 21:28:42 +0000

    fix: Task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movie_id = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/:${movie_id}`;
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(url, (err, res, body) => {
   if (err) {
     console.error(err);
   }
   const json = JSON.parse(body);
-  console.log(json);
+  console.log(json.title);
 });
